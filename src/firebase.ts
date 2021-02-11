67af046220bd1c3179712660181e3d9593eca4e0
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-02-11 21:26:36 +0900

    remove hard coded env vars

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,21 @@
 import firebase from 'firebase';
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyATdsBNt5WWBfffNHJbGuBeG-mZzyfM2go',
-  authDomain: 'rtf-101-chatapp.firebaseapp.com',
-  projectId: 'rtf-101-chatapp',
-  storageBucket: 'rtf-101-chatapp.appspot.com',
-  messagingSenderId: '608542598081',
-  appId: '1:608542598081:web:c76d7aed678c24a8e30540',
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
