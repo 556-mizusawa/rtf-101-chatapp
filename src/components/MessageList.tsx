eb87e9477d0a54c73ec26d4544c0e8aa4bd17389
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-02-12 20:51:47 +0900

    fetch messages

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, SetStateAction, useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+import { messagesRef } from '../firebase';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -10,7 +12,27 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const MessageList: React.FC<{}> = () => {
+  const [messages, setMessages] = useState<SetStateAction<string | unknown>>(
+    []
+  );
   const classes = useStyles();
+
+  useEffect(() => {
+    messagesRef
+      .orderByKey()
+      .limitToLast(3)
+      .on('value', (snapshot) => {
+        const messages = snapshot.val();
+        if (messages === null) return;
+        const entries = Object.entries(messages);
+        const newMessages = entries.map((entry) => {
+          const [key, nameAndText]: any = entry;
+          return { key, ...nameAndText };
+        });
+        setMessages(newMessages);
+      });
+  }, []);
+
   return <div className={classes.root}>MessageList</div>;
 };
 
