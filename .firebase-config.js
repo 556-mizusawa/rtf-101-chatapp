ed7c603f4e11465a3659a90b41fe316d2bcecb3a
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-02-24 11:42:54 +0900

    create command to generate .env.local

---
+++ .firebase-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyATdsBNt5WWBfffNHJbGuBeG-mZzyfM2go',
+  authDomain: 'rtf-101-chatapp.firebaseapp.com',
+  databaseURL: 'https://rtf-101-chatapp-default-rtdb.firebaseio.com',
+  projectId: 'rtf-101-chatapp',
+  storageBucket: 'rtf-101-chatapp.appspot.com',
+  messagingSenderId: '608542598081',
+  appId: '1:608542598081:web:c76d7aed678c24a8e30540',
+};
